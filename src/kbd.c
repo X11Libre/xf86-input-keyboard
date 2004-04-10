98ecec7dd9e12fe078076012af763d01a3cecd1b
Roland Mainz <roland.mainz@nrubsig.org>
2004-04-10 09:05:15 +0000

    Resync to 2004-04-10 XORG-RELEASE-1 branch

--- src/kbd.c
+++ src/kbd.c
@@ -1,4 +1,3 @@
-/* $XdotOrg: xc/programs/Xserver/hw/xfree86/input/keyboard/kbd.c,v 1.1.4.2.4.1 2004/03/04 17:48:05 eich Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/input/keyboard/kbd.c,v 1.8 2003/11/03 05:11:47 tsi Exp $ */
 
 /*
@@ -12,6 +11,7 @@
  * xf86Events.c and xf86Io.c which are
  * Copyright 1990,91 by Thomas Roell, Dinkelscherben, Germany.
  */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/input/keyboard/kbd.c,v 1.1.4.2.4.4 2004/04/06 20:47:49 eich Exp $ */
   
 #define NEED_EVENTS
 #include "X.h"
@@ -124,7 +124,7 @@ static const OptionInfoRec KeyboardOptions[] = {
 static const char *kbdDefaults[] = {
     "Protocol",		"standard",
     "AutoRepeat",	"500 30",
-    "XkbRules",		"xfree86",
+    "XkbRules",		__XKBDEFRULES__,
     "XkbModel",		"pc101",
     "XkbLayout",	"us",
     "Panix106",		"off",
@@ -759,7 +759,7 @@ static XF86ModuleVersionInfo xf86KeyboardVersionRec =
     MODULEVENDORSTRING,
     MODINFOSTRING1,
     MODINFOSTRING2,
-    XF86_VERSION_CURRENT,
+    XORG_VERSION_CURRENT,
     1, 0, 0,
     ABI_CLASS_XINPUT,
     ABI_XINPUT_VERSION,
